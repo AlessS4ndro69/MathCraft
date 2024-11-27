5e497aea5b85d90244091dbadae2872de6b9f5e7
JD17VM <jdiegodiego4@gmail.com>
2024-11-27 14:28:02 -0500

    .

--- static/js/navegador.js
+++ static/js/navegador.js
@@ -11,3 +11,18 @@
      navbar.classList.remove('reducida');
    }
  });
+
+
+
+ const boton = document.getElementById('boton_hamburguesa');
+ const cont_menu = document.getElementById('menu');
+ 
+
+ 
+ boton_hamburguesa.addEventListener('click', () => {
+     if (cont_menu.style.display === 'none') {
+         cont_menu.style.display = 'flex'; // Mostrar el div
+     } else {
+         cont_menu.style.display = 'none'; // Ocultar el div
+     }
+ });
